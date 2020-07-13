1804e87f5542de96461ce0cb2c312fcd55bfc26a
Reema Gowda <reemagowdan@gmail.com>
2020-07-13 15:00:54 +0530

    Mongodb database creation

--- backend/App.js
+++ backend/App.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express();
 
+
+
+
+const morgan = require('morgan')
+const cors = require('cors')
+//extended the json data and make easy to read bodypraser
+const bodyPraser = require('body-parser')
+//mongoose make very easy to connect to mongodb database
+ const mongoose = require('mongoose')
+
+
+app.use(morgan('dev'))
+app.use(cors())
+//can post only simple object not nested object (so extended false)
+app.use(bodyPraser.urlencoded({ extended: false }))
+app.use(bodyPraser.json())
+
+
+
+//ROUTE
 // app.use('/', (req, res) => {
 //     res.send('App is running on port..')
 // })
 
 
 //SIGNUP ROUTE
-
 app.use('/api/signup', require('./route/signup'))
 
 
