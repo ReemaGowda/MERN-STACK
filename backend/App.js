d16970cbb5278b31e064fd6d7c66e6a32f0180a3
Reema Gowda <reemagowdan@gmail.com>
2020-07-13 14:38:01 +0530

    backend route for sigup

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
 
+// app.use('/', (req, res) => {
+//     res.send('App is running on port..')
+// })
 
-app.use('/', (req, res) => {
-    res.send('App is running on port')
-})
+
+//SIGNUP ROUTE
+
+app.use('/api/signup', require('./route/signup'))
+
+
+
+
 module.exports = app;
---
+++ backend/route/signup.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Message received from post on /api/signup'
+    })
+})
+
+
+module.exports = router;
